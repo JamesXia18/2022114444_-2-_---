5c0e0d1a28ca0b695fe98987b46da03c63c51221
91770 <91770@DESKTOP-AC7TDFJ>
2024-07-17 09:47:59 +0800

    添加项目文件。

---
+++ DbConnecter/DbConnecter/SqlConnectionManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Data.SqlClient;
+using System;
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class SqlConnectionManager
+{
+    private readonly string _connectionString;
+
+    public SqlConnectionManager(string connectionString)
+    {
+        _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+    }
+
+    public SqlConnection GetConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+
+    public void ExecuteNonQuery(string sql, params SqlParameter[] parameters)
+    {
+        using (SqlConnection connection = GetConnection())
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddRange(parameters);
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public SqlDataReader ExecuteReader(string sql, params SqlParameter[] parameters)
+    {
+        SqlConnection connection = GetConnection();
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddRange(parameters);
+        connection.Open();
+        return command.ExecuteReader();
+    }
+
+    public T ExecuteScalar<T>(string sql, params SqlParameter[] parameters)
+    {
+        using (SqlConnection connection = GetConnection())
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddRange(parameters);
+            connection.Open();
+            object result = command.ExecuteScalar();
+            return (T)Convert.ChangeType(result, typeof(T));
+        }
+    }
+
+    /// <summary>
+    /// 测试数据库是否正确连接
+    /// </summary>
+    /// <returns></returns>
+    public bool TestConnection()
+    {
+        try
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+                // 一旦 Open() 调用没有抛出异常，我们可以认为连接是成功的。
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            // 如果捕获到任何异常，可能是连接失败。
+            Console.WriteLine($"Connection test failed with error: {ex.Message}");
+            return false;
+        }
+    }
+}
---
+++ DbConnecter/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using static SqlConnectionManager;
+
+namespace Test
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=Localhost;Initial Catalog=master;User ID=sa;Password=123456";
+
+            SqlConnectionManager connectionManager = new SqlConnectionManager(connectionString);
+            if (connectionManager.TestConnection()) {
+                Console.WriteLine("连接成功");
+            }
+
+
+        }
+    }
+}
